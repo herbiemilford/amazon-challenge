b2690e2240a245a8e3579ebe9ad2540e3d1048c0
Joe Crowder <toast.one.art@gmail.com>
2020-09-26 13:26:49 -0700

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAh1cUq-EWAGPdY3R2JYgJdrOhLvMYYHjU",
+    authDomain: "challenge-41237.firebaseapp.com",
+    databaseURL: "https://challenge-41237.firebaseio.com",
+    projectId: "challenge-41237",
+    storageBucket: "challenge-41237.appspot.com",
+    messagingSenderId: "950817108232",
+    appId: "1:950817108232:web:8c6c6a6956104775213cbd",
+    measurementId: "G-PBQQNN6CXM"
+};
