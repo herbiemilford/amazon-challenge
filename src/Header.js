a2a75a0565363b4800267249baf5adfda55902af
Joe Crowder <toast.one.art@gmail.com>
2020-09-26 22:17:11 -0700

    header

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -12,7 +15,7 @@ function Header() {
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
-        {/* Logo */}
+        <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
@@ -27,6 +30,11 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+      <img alt="Amazon Original Utopia. Watch today on Prime Video." src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/TV/UPIA_S1_01599_GWBleedingHero_1500x600_POST_Final_en-US_PVD5365._CB406529186_.jpg" height="600px" width="1500px" data-a-hires="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/TV/UPIA_S1_01599_GWBleedingHero_3000x1200_POST_Final_en-US_PVD5365._CB406529187_.jpg"/>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
